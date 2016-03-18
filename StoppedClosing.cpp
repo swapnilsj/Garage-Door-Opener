9e5470007543db092e7c7a47111cda1296cd717d
swapnilsj <ssj3000@rit.edu>
2016-03-17 21:22:25 -0400

    deleted files
    Signed-off-by: swapnilsj <ssj3000@rit.edu>

--- StoppedClosing.cpp
+++
@@ -1,8 +0,0 @@
-#include "StoppedClosing.h"
-
-void StoppedClosing::Entry(){
-	std::cout << "Garage Door Closing\n" << std::endl;
-	reciever->setmotorDown(false);
-	reciever->setmotorUp(false);
-	reciever->setBeam(false);
-}
--- StoppedOpening.cpp
+++
@@ -1,9 +0,0 @@
-#include "StoppedOpening.h"
-
-void StoppedOpening::Entry(){
-	std::cout << "Garage Door Stopped Opening, beam off\n" << std::endl;
-	reciever->setmotorDown(false);
-	reciever->setmotorUp(false);
-	reciever->setBeam(false);
-}
-
